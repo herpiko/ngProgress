dbcf08e302ec1e9f1e10c4c670f647724ce0b6ad
Herpiko Dwi Aguno <herpiko@gmail.com>
2015-10-19 01:36:05 +0700

    Use module.exports for npm compatibility

--- src/module.js
+++ src/module.js
@@ -1,2 +1,3 @@
 // Here we define the maim module for easy dependency injection into other modules
 angular.module('ngProgress', ['ngProgress.directive', 'ngProgress.provider']);
+module.exports = angular;
